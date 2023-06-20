1b2a0b2bd4fbf70c2839e19f9e60f84c94ecdf90
Harshit2229 <harshittrivedi22022001@gmail.com>
2023-06-20 10:46:22 +0530

    image added to the screen

--- lib/My_bookings.dart
+++ lib/My_bookings.dart
@@ -19,7 +19,7 @@ class _My_bookingsState extends State<My_bookings> {
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Padding(
+            const Padding(
               padding: EdgeInsets.fromLTRB(30.0, 100.0, 20.0, 20.0),
               child: Center(
                 child: Text(
@@ -33,12 +33,12 @@ class _My_bookingsState extends State<My_bookings> {
               ),
             ),
             SizedBox(
-              width: 300.0,
+              width: 342.0,
               height: 48.0,
               child: Container(
                 decoration: BoxDecoration(
-                  color: Colors.grey,
-                  borderRadius: BorderRadius.circular(10.0),
+                  color: const Color(0xFFF1F1F1), // Use the #F1F1F1 color
+                  borderRadius: BorderRadius.circular(20.0),
                 ),
                 child: Row(
                   children: [
@@ -50,20 +50,23 @@ class _My_bookingsState extends State<My_bookings> {
                             _isButton2Selected = false;
                           });
                         },
-                        child: Container(
-                          width: 100.0, // Adjust the width as desired
-                          height: 32.0, // Adjust the height as desired
-                          decoration: BoxDecoration(
-                            color: _isButton1Selected ? Colors.blue : Colors.transparent,
-                            borderRadius: BorderRadius.circular(10.0),
-                          ),
-                          child: Center(
-                            child: Text(
-                              'Button 1',
-                              style: TextStyle(
-                                color: _isButton1Selected ? Colors.white : Colors.black,
-                                fontSize: 18.0,
-                                fontWeight: FontWeight.bold,
+                        child: Padding(
+                          padding: const EdgeInsets.only(left: 10.0),
+                          child: Container(
+                            width: 164.0,
+                            height: 32.0,
+                            decoration: BoxDecoration(
+                              color: _isButton1Selected ? Colors.blue : Colors.transparent,
+                              borderRadius: BorderRadius.circular(20.0),
+                            ),
+                            child: Center(
+                              child: Text(
+                                'Upcoming',
+                                style: TextStyle(
+                                  color: _isButton1Selected ? Colors.white : Colors.black,
+                                  fontSize: 18.0,
+                                  fontWeight: FontWeight.bold,
+                                ),
                               ),
                             ),
                           ),
@@ -79,20 +82,23 @@ class _My_bookingsState extends State<My_bookings> {
                             _isButton2Selected = !_isButton2Selected;
                           });
                         },
-                        child: Container(
-                          width: 100.0, // Adjust the width as desired
-                          height: 32.0,
-                          decoration: BoxDecoration(
-                            color: _isButton2Selected ? Colors.blue : Colors.transparent,
-                            borderRadius: BorderRadius.circular(10.0),
-                          ),
-                          child: Center(
-                            child: Text(
-                              'Button 2',
-                              style: TextStyle(
-                                color: _isButton2Selected ? Colors.white : Colors.black,
-                                fontSize: 18.0,
-                                fontWeight: FontWeight.bold,
+                        child: Padding(
+                          padding: const EdgeInsets.only(right: 10.0),
+                          child: Container(
+                            width: 164.0,
+                            height: 32.0,
+                            decoration: BoxDecoration(
+                              color: _isButton2Selected ? Colors.blue : Colors.transparent,
+                              borderRadius: BorderRadius.circular(20.0),
+                            ),
+                            child: Center(
+                              child: Text(
+                                'Completed',
+                                style: TextStyle(
+                                  color: _isButton2Selected ? Colors.white : Colors.black,
+                                  fontSize: 18.0,
+                                  fontWeight: FontWeight.bold,
+                                ),
                               ),
                             ),
                           ),
@@ -102,6 +108,92 @@ class _My_bookingsState extends State<My_bookings> {
                   ],
                 ),
               ),
+            ),
+            const SizedBox(height: 30.0),
+            Container(
+              width: 342.0,
+              height: 158.0,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10.0),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.3),
+                    spreadRadius: 2.0,
+                    blurRadius: 5.0,
+                    offset: const Offset(0, 3),
+                  ),
+                ],
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Row(
+                  children: [
+                    Container(
+                      width: 80.0,
+                      height: 80.0,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10.0),
+                        color: Colors.blue,
+                      ),
+                      child: Image.asset('assets/Washing.png'),
+                    ),
+                    const SizedBox(width: 16.0),
+                    const Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Booking Information',
+                            style: TextStyle(
+                              fontSize: 14.0,
+                            ),
+                          ),
+                          SizedBox(height: 8.0),
+                          Row(
+                            children: [
+                              Icon(Icons.add_circle_outline, size: 20.0, color: Colors.black),
+                              SizedBox(width: 4.0),
+                              Text(
+                                'Car',
+                                style: TextStyle(fontSize: 14.0),
+                              ),
+                            ],
+                          ),
+                          SizedBox(height: 4.0),
+                          Row(
+                            children: [
+                              Icon(Icons.fmd_good_outlined, size: 20.0, color: Colors.black),
+                              SizedBox(width: 4.0),
+                              Text(
+                                'Map',
+                                style: TextStyle(fontSize: 14.0),
+                              ),
+                            ],
+                          ),
+                          SizedBox(height: 4.0),
+                          Row(
+                            children: [
+                              Icon(Icons.calendar_today, size: 20.0, color: Colors.black),
+                              SizedBox(width: 4.0),
+                              Text(
+                                'Calendar',
+                                style: TextStyle(fontSize: 14.0),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                    const Icon(
+                      Icons.arrow_forward_ios,
+                      color: Colors.grey,
+                      size: 20.0,
+                    ),
+                  ],
+                ),
+              ),
+
             ),
           ],
         ),
